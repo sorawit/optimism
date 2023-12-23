cb32a90d05f49d601db4c8183188c18b26a68c81
Sorawit Suriyakarn <thepsint@gmail.com>
2023-12-23 08:32:48 +0000

    Fix IsL2DevPredeploy

--- op-chain-ops/genesis/helpers.go
+++ op-chain-ops/genesis/helpers.go
@@ -74,7 +74,8 @@ func AddressToCodeNamespace(addr common.Address) (common.Address, error) {
 }
 
 func IsL2DevPredeploy(addr common.Address) bool {
-	return bytes.Equal(addr[0:2], []byte{0x42, 0x00, 0x11})
+	return bytes.Equal(addr[0:2], []byte{0x42, 0x00}) ||
+		bytes.Equal(addr[0:2], []byte{0x11, 0x00})
 }
 
 // GetBlockFromTag will resolve a Block given an rpc block tag
